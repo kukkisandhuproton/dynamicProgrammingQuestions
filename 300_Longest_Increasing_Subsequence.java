1f0f6c823a533081381bacd1a3ba05c10f1aae0e
kukkisandhuproton <131423888+kukkisandhuproton@users.noreply.github.com>
2023-04-26 21:58:01 -0400

    Update 300_Longest_Increasing_Subsequence.java

--- 300_Longest_Increasing_Subsequence.java
+++ 300_Longest_Increasing_Subsequence.java
@@ -28,3 +28,24 @@ class Solution {
     }
 
 }
+
+// Long sequene
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int[]dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        int result = 1;
+        for(int i=0;i<nums.length;i++){//index
+            for(int j=0;j<i;j++){//previous
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            result=Math.max(result,dp[i]);
+        }
+        return result;
+    }
+}
