b4ae3c80f25f8cbca8cc73ff6f32a903c48d1700
kukkisandhuproton <131423888+kukkisandhuproton@users.noreply.github.com>
2023-04-26 21:52:22 -0400

    Update 368_Largest_Divisible_Subset.java

--- 368_Largest_Divisible_Subset.java
+++ 368_Largest_Divisible_Subset.java
@@ -26,3 +26,38 @@ class Solution {
         solve(nums,index+1,previous,answer);
     }
 }
+
+// Second solution
+
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int[] result = new int[nums.length];
+        return construct(nums,result,getLDSize(nums,result));        
+    }
+    public int getLDSize(int[]nums,int[]result){
+        Arrays.sort(nums);
+        Arrays.fill(result,1);
+        int ldsize=1;
+        for(int i=0;i<nums.length;i++){//index
+            for(int j=0;j<i;j++){//previous
+                if(nums[i]%nums[j]==0){
+                    result[i]=Math.max(result[i],1+result[j]);
+                    ldsize=Math.max(ldsize,result[i]);
+                }
+            }
+        }
+        return ldsize;
+    }
+    public List<Integer> construct(int[]nums,int[]result,int ldsize){
+        int previous = -1;
+        var lds = new LinkedList<Integer>();
+        for(int i=result.length-1;i>=0;i--){
+            if(result[i]==ldsize&&(previous==-1||previous%nums[i]==0)){
+                lds.addFirst(nums[i]);
+                ldsize--;
+                previous=nums[i];
+            }
+        }
+        return lds;
+    }
+}
